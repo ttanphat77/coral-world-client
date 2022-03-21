7c4bbc8b7a2755811c8289ba31a1667ce64facf4
phattt <ttanphat77@gmail.com>
2022-03-22 02:15:08 +0700

    fix build

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,27 @@ import Home from "./pages/home"
 import Taxonomy from "./pages/taxonomy"
 import About from "./pages/about";
 import {Routes, BrowserRouter, Route, Outlet} from "react-router-dom";
+import {Box} from "@chakra-ui/react";
 
 export const App = () => (
     <BrowserRouter
         // basename={process.env.PUBLIC_URL}
     >
-        <Routes>
-            <Route path='/' element={<Layout/>}>
-                <Route index element={<Home/>}/>
-                {/*<Route path='/taxonomy' element={<Taxonomy/>}/>*/}
-                {/*<Route path='/about' element={<About/>}/>                                                                                                                                                       */}
-            </Route>
-        </Routes>
+        <Header/>
+        <hr/>
+            <Box style={{margin: '75px 0 0 0'}}>
+                <Routes>
+                    <Route path={'/'} element={<Home/>}/>
+                    <Route path={'/taxonomy'} element={<Taxonomy/>}/>
+                    <Route path={'/about'} element={<About/>}/>
+                </Routes>
+            </Box>
     </BrowserRouter>
 )
 
 function Layout() {
     return (
         <div>
-            <Header/>
-            <hr/>
-            <div style={{margin: '75px 0 0 0'}}>
-                <Outlet/>
-            </div>
         </div>
     )
 }
